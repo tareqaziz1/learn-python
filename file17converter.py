aab7a021bf5fba74760fa5319d4967a2079f40b2
tareqaziz1 <tareqazeez1@gmail.com>
2026-03-09 23:48:46 +0100

    file17converter added

---
+++ file17converter.py
@@ -0,0 +1,15 @@
+# This file contains 2 functions that will be used in another file (file17) as a module.
+
+def lbs_to_kg(weight):
+     in_kilogram = weight * 0.4535
+     return in_kilogram
+
+
+def kg_to_lbs(weight):
+    in_pounds = weight / 0.4535
+    return in_pounds
+
+
+
+
+
