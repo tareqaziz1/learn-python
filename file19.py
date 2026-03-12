33aa8b9f36667eb5301b196616d92fb02a720b56
tareqaziz1 <tareqazeez1@gmail.com>
2026-03-12 23:39:08 +0100

    file19 updated

--- file19.py
+++ file19.py
@@ -1,1 +1,9 @@
-#packages
+#Directories and files
+
+#Absolute path : The full path to a file or folder starting from the root directory.
+#Relative path : The path to a file or folder based on the current working directory.
+
+
+
+
+from pathlib import Path
