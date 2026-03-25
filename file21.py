a4dc46ed2da3522669a1aa37ae8bb5685fcbd295
tareqaziz1 <tareqazeez1@gmail.com>
2026-03-25 23:23:45 +0100

    file21 updated

--- file21.py
+++ file21.py
@@ -3,9 +3,9 @@
 import openpyxl as xl
 
 path = ("./others/transactions.xlsx")
+wordbook = xl.load_workbook(path)
+sheet = wordbook["Sheet1"]
 
-workbook = xl.load_workbook(path)
-
-sheet = workbook['Sheet1']
-
+for row in range(1, sheet.max_row + 1):
+    print(row)
 
