46cf8b3dc9b4150f0f73ca8556aab5764ee69846
tareqaziz1 <tareqazeez1@gmail.com>
2026-03-31 04:27:41 +0200

    file21 updated

--- file21.py
+++ file21.py
@@ -10,3 +10,8 @@ for row in range(1, sheet.max_row + 1):
     cell = sheet.cell(row, 1)
     print(cell.value)
 
+for row in sheet.iter_rows(values_only=True):
+    print(row)
+
+sheet.cell(row=1, column=4).value = "increased_price"
+print(sheet.cell(row=1,column=4).value)
