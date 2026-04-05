8040e39aa16947aced5c1ad1de491e31210afbaa
tareqaziz1 <tareqazeez1@gmail.com>
2026-04-05 04:45:58 +0200

    file21 updated

--- file21.py
+++ file21.py
@@ -1,4 +1,4 @@
-# Excel
+# Excel operations
 
 import openpyxl as xl
 
@@ -10,16 +10,27 @@ for row in range(1, sheet.max_row + 1):
     cell = sheet.cell(row, 2)
     print(cell.value)
 
-# for row in sheet.iter_rows(values_only=True):
 
-sheet.cell(row=1, column=4).value = "Shipping Cost"
+# Add a column as shipping cost
+
+sheet.cell(row = 1, column = 4).value = "shipping_cost"
 
 for row in range(2, sheet.max_row + 1):
     shipping = 5.5
-    shipping_cost = sheet.cell(row, 4).value = shipping
-    print(shipping_cost)
+    sheet.cell(row, 4).value = shipping
+
+# Add a column as total cost. total cost will be - price + shipping cost
+
+sheet.cell(row = 1, column = 5).value = "total_cost"
+
+for row in range(2, sheet.max_row + 1):
+    cell_price = sheet.cell(row, 3).value
+    cell_shipping = sheet.cell(row, 4).value
+    total_cost = cell_price + cell_shipping
+    total_cost_cell = sheet.cell(row, 5)
+    total_cost_cell.value = total_cost
+    print(total_cost_cell.value)
+
 
 for row in sheet.iter_rows(values_only=True):
     print(row)
-
-
