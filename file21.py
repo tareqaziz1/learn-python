8340fde13f39129cf5f3c0e6829a2fba649fd2db
tareqaziz1 <tareqazeez1@gmail.com>
2026-04-05 04:27:29 +0200

    file21 updated

--- file21.py
+++ file21.py
@@ -3,17 +3,23 @@
 import openpyxl as xl
 
 path = ("./others/transactions.xlsx")
-wordbook = xl.load_workbook(path)
-sheet = wordbook["Sheet1"]
+workbook = xl.load_workbook(path)
+sheet = workbook["Sheet1"]
 
 for row in range(1, sheet.max_row + 1):
-    cell = sheet.cell(row, 1)
+    cell = sheet.cell(row, 2)
     print(cell.value)
+
+# for row in sheet.iter_rows(values_only=True):
+
+sheet.cell(row=1, column=4).value = "Shipping Cost"
+
+for row in range(2, sheet.max_row + 1):
+    shipping = 5.5
+    shipping_cost = sheet.cell(row, 4).value = shipping
+    print(shipping_cost)
 
 for row in sheet.iter_rows(values_only=True):
     print(row)
-
-sheet.cell(row=1, column=4).value = "increased_price" # new header added
-
 
 
