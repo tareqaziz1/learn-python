ee1512795ceb98a2f7e7a9575b10542f2540dd9f
tareqaziz1 <tareqazeez1@gmail.com>
2026-04-09 23:35:10 +0200

    file22 updated

--- file22.py
+++ file22.py
@@ -1,5 +1,21 @@
 # Charts with openpyxl
 
 import openpyxl as xl
-from openpyxl.chart import BarChart, Reference
+from openpyxl.chart import BarChart, LineChart, PieChart, Reference
 
+path = "./others/transactions_updated_v2.xlsx"
+workbook = xl.load_workbook(path)
+sheet = workbook["Sheet1"]
+
+bar_chart = BarChart()
+
+data = Reference(sheet, min_col=3, min_row=1, max_row=sheet.max_row)
+category = Reference(sheet, min_col=2, min_row=2, max_row=sheet.max_row)
+
+bar_chart.add_data(data, titles_from_data=True)
+bar_chart.set_categories(category)
+bar_chart.title = "Price per product"
+sheet.add_chart(bar_chart, "A25")
+
+
+#workbook.save("./others/transactions_updated_v3.xlsx")
